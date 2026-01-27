8f687c4121ea6bcacdda6e7e92b910c425e60b0d
qingzhixing <qzsqqmail@qq.com>
2026-01-27 18:55:10 +0800

    💖 E. Product Queries

--- Codeforces Round 1076 (Div. 3)/E_Product_Queries.cpp
+++ Codeforces Round 1076 (Div. 3)/E_Product_Queries.cpp
@@ -1,18 +1,54 @@
 #include <iostream>
 #include <vector>
+#include <set>
 #include <algorithm>
 using namespace std;
 
+const int MAX_N = 3e5 + 10;
+const int IMPOSSIBLE = 114514191;
+
+int n;
+int dp[MAX_N];
+
 void Solution()
 {
-	int n;
 	cin >> n;
-	vector<int> a(n + 10);
+
 	for (int i = 1; i <= n; i++)
 	{
-		cin >> a[i];
+		dp[i] = IMPOSSIBLE;
 	}
-	sort(a.begin() + 1, a.begin() + 1 + n);
+
+	for (int i = 1; i <= n; i++)
+	{
+		int num;
+		cin >> num;
+		dp[num] = 1;
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		int times = 1;
+		while (i * times <= n)
+		{
+			int target_number = i * times;
+			dp[target_number] = min(dp[target_number], dp[i] + dp[times]);
+			times++;
+		}
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (dp[i] == IMPOSSIBLE)
+		{
+			cout << -1 << " ";
+		}
+		else
+		{
+			cout << dp[i] << " ";
+		}
+	}
+	cout << endl;
 }
 
 int main()
