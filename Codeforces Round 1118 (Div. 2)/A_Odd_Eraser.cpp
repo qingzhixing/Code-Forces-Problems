97c8eb8c42f121a3a9eb3dfc52d07f6d9a52e65e
qingzhixing <qzsqqmail@qq.com>
2026-08-29 23:18:18 +0800

    📦 NEW: A_Odd_Eraser

--- Codeforces Round 1118 (Div. 2)/A_Odd_Eraser.cpp
+++ Codeforces Round 1118 (Div. 2)/A_Odd_Eraser.cpp
@@ -1,1 +1,38 @@
-#error TODO
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+	if (b == 0)
+	{
+		return a;
+	}
+	return gcd(b, a % b);
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		int a, b;
+		int num;
+		for (int i = 1; i <= n; i++)
+		{
+			cin >> num;
+			if (i == 1)
+			{
+				a = num;
+			}
+			if (i == n)
+			{
+				b = num;
+			}
+		}
+		cout << gcd(a, b) << endl;
+	}
+	return 0;
+}
