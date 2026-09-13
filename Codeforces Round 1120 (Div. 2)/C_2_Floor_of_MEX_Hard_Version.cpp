1fb4b46a60e6d3fe380bbd239fc25d971dc378af
qingzhixing <qzsqqmail@qq.com>
2026-09-13 09:43:08 +0800

    📦 NEW: C2 补题标记

---
+++ Codeforces Round 1120 (Div. 2)/C_2_Floor_of_MEX_Hard_Version.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_N = 0;
+const int MAX_M = 0;
+
+#error TODO: Uncomplemented.
+
+void Solution()
+{
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Solution();
+	}
+	return 0;
+}
